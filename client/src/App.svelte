2134e4e9b0fb44fbe02192f297027c22f0733a6f
Skyler <goodsky6@gmail.com>
2025-12-29 10:21:49 -0800

    Placeholder app content

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,20 @@
-<h1>Hello World</h1>
+<script>
+    import { onMount } from 'svelte';
+    import Connections from './Connections.svelte';
+  
+    let isValidPath = true;
+  
+    onMount(() => {
+        // We only support the root path
+        isValidPath = window.location.pathname === '/';
+    });
+</script>
+
+{#if isValidPath}
+    <Connections />
+{:else}
+    <div style="text-align: center; padding: 2rem;">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </div>
+{/if}
